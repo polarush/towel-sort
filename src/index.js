8b24760e4dca380ba2cb79853aba76eb8a109a93
Yevgeniy Polyarush <polyarush.e@mail.ru>
2021-08-18 17:20:54 +0400

    feat: add main function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,24 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+		
+	if (matrix) {
+		let result = [];
+		for (let i = 0; i < matrix.length; i++) { // переворачиваем каждый чётный массив
+			if ((i % 2) !== 0) {
+			  matrix[i] = matrix[i].reverse(); 
+  			}
+    	}
+
+  		for (let i = 0; i < matrix.length; i++) {    // сливаем массивы в один
+			  for (let j = 0; j < matrix[i].length; j++) {
+				  result.push(matrix[i][j])
+			  }
+  		}
+  	return result
+	}
+	else {
+		return []
+	}
+
 }
